e29961793e26cb944a6441ebfe5afae66205e52d
sharvaribhor <bhormadhuri08@gmail.com>
2022-01-03 16:43:08 +0530

    madhuri

---
+++ day1_function.js
@@ -0,0 +1,37 @@
+let x=10
+console.log(x)
+
+let y=20
+console.log(x+y)
+
+//function declaration
+
+function add(a,b){
+    console.log(a+b)
+}
+add(10,20)
+console.log("-----------------------------------------------")
+add(30,30)
+
+//function expression
+
+let x1=function(){
+    console.log(20-10)
+}
+
+x1()
+
+//arrow function
+let y1=()=>{
+    console.log(9*8)
+}
+y1()
+
+
+function sum(u,v){
+    return u+v
+}
+let w=sum(10,20)
+console.log(w)
+
+
